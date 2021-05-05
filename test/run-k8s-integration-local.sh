bb2737162a7c2d5757d4d60bbba017c03101a418
Matthew Cary <mattcary@google.com>
2021-05-05 14:56:11 -0700

    transfer files between workstations
    Change-Id: Ib2d9c9203814c43fe548cfcab93fe007a05c65a5

--- test/run-k8s-integration-local.sh
+++ test/run-k8s-integration-local.sh
@@ -25,12 +25,13 @@ make -C "${PKGDIR}" test-k8s-integration
 # This version of the command creates a GCE cluster. It downloads and builds two k8s releases,
 # one for the cluster and one for the tests, unless the cluster and test versioning is the same.
 
-# ${PKGDIR}/bin/k8s-integration-test --run-in-prow=false \
-# --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
-# --deploy-overlay-name=dev --storageclass-files=sc-standard.yaml,sc-balanced.yaml,sc-ssd.yaml \
-# --test-focus="External.Storage" --gce-zone="us-central1-b" \
-# --deployment-strategy=gce --kube-version=${kube_version} \
-# --test-version=${test_version} --num-nodes=3
+${PKGDIR}/bin/k8s-integration-test --run-in-prow=false \
+--service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json --do-driver-build=false \
+--deploy-overlay-name=stable-master --storageclass-files=sc-standard.yaml,sc-balanced.yaml,sc-ssd.yaml \
+--test-focus="External.Storage" --gce-zone="us-central1-b" \
+--deployment-strategy=gce \
+--local-k8s-dir=/clank/go/src/k8s.io/kubernetes \
+--num-nodes=3
 
 
 # This version of the command creates a regional GKE cluster. It will test with
@@ -84,10 +85,10 @@ make -C "${PKGDIR}" test-k8s-integration
 #
 # As with all other methods local credentials must be set by running 
 #  gcloud auth application-default login
-"${PKGDIR}/bin/k8s-integration-test" --run-in-prow=false \
---deploy-overlay-name=noauth --bringup-cluster=false --teardown-cluster=false --local-k8s-dir="$KTOP" \
---storageclass-files=sc-standard.yaml --do-driver-build=false --test-focus='External.Storage' \
---gce-zone="us-central1-b" --num-nodes="${NUM_NODES:-3}"
+# "${PKGDIR}/bin/k8s-integration-test" --run-in-prow=false \
+# --deploy-overlay-name=noauth --bringup-cluster=false --teardown-cluster=false --local-k8s-dir="$KTOP" \
+# --storageclass-files=sc-standard.yaml --do-driver-build=false --test-focus='External.Storage' \
+# --gce-zone="us-central1-b" --num-nodes="${NUM_NODES:-3}"
 
 
 # This version of the command does not build the driver or K8s, points to a
